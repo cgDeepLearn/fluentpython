393a67677bde8d9f0d3e59860bee2c384b0fdee6
cgDeepLearn <cglearningnow@163.com>
2017-12-28 16:43:36 +0800

    add generator

---
+++ chapter14-it-generator/sentence_gen.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+使用生成器函数实现Sentence类
+"""
+
+
+import re
+import reprlib
+
+RE_WORD = re.compile(r'\w+')
+
+
+class Sentence:
+
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        for word in self.words:
+            yield word
+        return
